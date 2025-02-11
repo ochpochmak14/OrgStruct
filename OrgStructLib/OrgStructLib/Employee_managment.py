1ed4459d5bfc147709c97375003dc7b93df9d53d
Emin <makhmudovemin441@gmail.com>
2025-02-11 19:27:40 +0500

    sketch of employee managment file

---
+++ OrgStructLib/OrgStructLib/Employee_managment.py
@@ -0,0 +1,34 @@
+class Employee_Managment:
+    def Add_employee(self) -> None:
+        """Adding employee to the Company"""
+        pass
+    
+    
+    def List_employees(self) -> list:
+        """Returns a List of employees in Company"""
+        pass
+    
+    
+    def Promote_employee(self) -> None:
+        """Promotes employee"""
+        pass
+    
+    
+    def Demote_employee(self) -> None:
+        """Demotes employee"""
+        pass
+    
+    
+    def Find_employee(self) -> str:
+        """Finds employee in Company"""
+        pass
+    
+    
+    def Update_employee_info(self) -> None:
+        """Updates employee info. E.g salary, position"""
+        pass
+    
+    
+    def Transfer_employee(self) -> None:
+        """Changes employee department"""
+        pass
--- OrgStructLib/OrgStructLib/main.py
+++ OrgStructLib/OrgStructLib/main.py
@@ -7,14 +7,7 @@ class Company:
         self.founders = founders
         self.creation_time = current_date
     
-    def Add_employee(self) -> None:
-        """Adding employee to the Company"""
-        pass
     
-    
-    def List_employees(self) -> list:
-        """Returns a List of employees in Company"""
-        pass
     
     
     def Roadmap(self) -> str:
@@ -27,35 +20,9 @@ class Company:
         pass
     
     
-    def Promote_employee(self) -> None:
-        """Promotes employee"""
-        pass
-    
-    
-    def Demote_employee(self) -> None:
-        """Demotes employee"""
-        pass
-    
-    
-    def Update_employee_info(self) -> None:
-        """Updates employee info. E.g salary, position"""
-        pass
-    
-    
-    def Find_employee(self) -> str:
-        """Finds employee in Company"""
-        pass
-    
-    
     def Create_department(self) -> None:
         """Creates department in Company"""
         pass
-    
-    
-    def Transfer_employee(self) -> None:
-        """Changes employee department"""
-        pass
-    
     
     def List_department(self) -> list:
         """Returns list of departments in Company"""
