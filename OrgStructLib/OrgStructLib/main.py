62e6b1abaf889062d8fabf4339ed6d724c08066e
Emin <makhmudovemin441@gmail.com>
2025-02-11 20:21:57 +0500

    finsih implementing DepartmentManagment

--- OrgStructLib/OrgStructLib/main.py
+++ OrgStructLib/OrgStructLib/main.py
@@ -3,29 +3,20 @@ from datetime import datetime
 current_date = datetime.now().date()
 class Company:
     def __init__(self, company_title: str, founders: list):
+        """Data about initialized Company
+        company_title,
+        founders,
+        creation_time"""
         self.company_title = company_title
         self.founders = founders
         self.creation_time = current_date
-    
-    
-    
     
     def Roadmap(self) -> str:
         """Returns a Company Roadmap"""
         pass
     
-    
     def Company_achievements(self) -> list:
         """Describing company_achievements"""
-        pass
-    
-    
-    def Create_department(self) -> None:
-        """Creates department in Company"""
-        pass
-    
-    def List_department(self) -> list:
-        """Returns list of departments in Company"""
         pass
     
     
