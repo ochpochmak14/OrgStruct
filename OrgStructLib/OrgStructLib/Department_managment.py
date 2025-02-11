dffdcfd5dedf98f286d3c7c69c6be9e899e03c4c
Emin <makhmudovemin441@gmail.com>
2025-02-11 20:30:36 +0500

    finish implementing DepartmentManagement file

---
+++ OrgStructLib/OrgStructLib/Department_managment.py
@@ -0,0 +1,26 @@
+from Employee_managment import EmployeeManagement
+
+
+class DepartmentManagment(EmployeeManagement):
+    Department_id = 0
+
+    def __init__(self):
+        super().__init__()
+        self.department_list = []
+
+    def Create_department(self, department_name: str) -> None:
+        """Creates department in Company
+        id;
+        title;
+        employees = []"""
+        DepartmentManagment.Department_id += 1
+        Department = {
+            "id": DepartmentManagment.Department_id,
+            "title": department_name,
+            "employees": [],
+        }
+        self.department_list.append(Department)
+
+    def List_department(self) -> list:
+        """Returns list of departments in Company"""
+        return self.department_list
